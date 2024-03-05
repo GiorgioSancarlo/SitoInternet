bb0ffab6717172e3f69f3586f6837d409f178505
giorgio sancarlo <sancarlogiorgio@gmail.com>
2024-03-05 09:52:09 +0100

    firstCommit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import {} from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Giorgio Sancarlo</h1>
+
+      <p className="read-the-docs">
+        Puoi togliere il ragazzo dallo Sperone ma non puoi togliere lo Sperone
+        dal ragazzo.
+      </p>
+    </>
+  );
+}
+
+export default App;
